a81d2ad436dfe1e3746f7a93632b1a487f30c1de
Diego Piedra <dy_tv@hotmail.com>
2019-11-29 14:12:00 -0500

    third commit

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -316,20 +316,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <!-- sidebar menu start-->
         <ul class="sidebar-menu">
           <li class="active">
-            <a class="" href="index.html">
+            <a class="" href="<?= base_url('');?>index.php/Welcome">
                           <i class="icon_house_alt"></i>
-                          <span>Dashboard</span>
+                          <span>Inicio</span>
                       </a>
           </li>
           <li class="sub-menu">
             <a href="javascript:;" class="">
                           <i class="icon_document_alt"></i>
-                          <span>Forms</span>
+                          <span>Sensores</span>
                           <span class="menu-arrow arrow_carrot-right"></span>
                       </a>
             <ul class="sub">
-              <li><a class="" href="form_component.html">Form Elements</a></li>
-              <li><a class="" href="form_validation.html">Form Validation</a></li>
+              <li><a class="" href="<?= base_url('');?>form_component.html">Sensor A</a></li>
+              <li><a class="" href="<?= base_url('');?>form_validation.html">Sensor B</a></li>
             </ul>
           </li>
           <li class="sub-menu">
@@ -1062,46 +1062,46 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <!-- container section start -->
 
   <!-- javascripts -->
-  <script src="js/jquery.js"></script>
-  <script src="js/jquery-ui-1.10.4.min.js"></script>
-  <script src="js/jquery-1.8.3.min.js"></script>
-  <script type="text/javascript" src="js/jquery-ui-1.9.2.custom.min.js"></script>
+  <script src="<?= base_url('');?>js/jquery.js"></script>
+  <script src="<?= base_url('');?>js/jquery-ui-1.10.4.min.js"></script>
+  <script src="<?= base_url('');?>js/jquery-1.8.3.min.js"></script>
+  <script type="text/javascript" src="<?= base_url('');?>js/jquery-ui-1.9.2.custom.min.js"></script>
   <!-- bootstrap -->
-  <script src="js/bootstrap.min.js"></script>
+  <script src="<?= base_url('');?>js/bootstrap.min.js"></script>
   <!-- nice scroll -->
-  <script src="js/jquery.scrollTo.min.js"></script>
-  <script src="js/jquery.nicescroll.js" type="text/javascript"></script>
+  <script src="<?= base_url('');?>js/jquery.scrollTo.min.js"></script>
+  <script src="<?= base_url('');?>js/jquery.nicescroll.js" type="text/javascript"></script>
   <!-- charts scripts -->
-  <script src="assets/jquery-knob/js/jquery.knob.js"></script>
-  <script src="js/jquery.sparkline.js" type="text/javascript"></script>
-  <script src="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
-  <script src="js/owl.carousel.js"></script>
+  <script src="<?= base_url('');?>assets/jquery-knob/js/jquery.knob.js"></script>
+  <script src="<?= base_url('');?>js/jquery.sparkline.js" type="text/javascript"></script>
+  <script src="<?= base_url('');?>assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
+  <script src="<?= base_url('');?>js/owl.carousel.js"></script>
   <!-- jQuery full calendar -->
-  <<script src="js/fullcalendar.min.js"></script>
+  <<script src="<?= base_url('');?>js/fullcalendar.min.js"></script>
     <!-- Full Google Calendar - Calendar -->
-    <script src="assets/fullcalendar/fullcalendar/fullcalendar.js"></script>
+    <script src="<?= base_url('');?>assets/fullcalendar/fullcalendar/fullcalendar.js"></script>
     <!--script for this page only-->
-    <script src="js/calendar-custom.js"></script>
-    <script src="js/jquery.rateit.min.js"></script>
+    <script src="<?= base_url('');?>js/calendar-custom.js"></script>
+    <script src="<?= base_url('');?>js/jquery.rateit.min.js"></script>
     <!-- custom select -->
-    <script src="js/jquery.customSelect.min.js"></script>
-    <script src="assets/chart-master/Chart.js"></script>
+    <script src="<?= base_url('');?>js/jquery.customSelect.min.js"></script>
+    <script src="<?= base_url('');?>assets/chart-master/Chart.js"></script>
 
     <!--custome script for all page-->
-    <script src="js/scripts.js"></script>
+    <script src="<?= base_url('');?>js/scripts.js"></script>
     <!-- custom script for this page-->
-    <script src="js/sparkline-chart.js"></script>
-    <script src="js/easy-pie-chart.js"></script>
-    <script src="js/jquery-jvectormap-1.2.2.min.js"></script>
-    <script src="js/jquery-jvectormap-world-mill-en.js"></script>
-    <script src="js/xcharts.min.js"></script>
-    <script src="js/jquery.autosize.min.js"></script>
-    <script src="js/jquery.placeholder.min.js"></script>
-    <script src="js/gdp-data.js"></script>
-    <script src="js/morris.min.js"></script>
-    <script src="js/sparklines.js"></script>
-    <script src="js/charts.js"></script>
-    <script src="js/jquery.slimscroll.min.js"></script>
+    <script src="<?= base_url('');?>js/sparkline-chart.js"></script>
+    <script src="<?= base_url('');?>js/easy-pie-chart.js"></script>
+    <script src="<?= base_url('');?>js/jquery-jvectormap-1.2.2.min.js"></script>
+    <script src="<?= base_url('');?>js/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="<?= base_url('');?>js/xcharts.min.js"></script>
+    <script src="<?= base_url('');?>js/jquery.autosize.min.js"></script>
+    <script src="<?= base_url('');?>js/jquery.placeholder.min.js"></script>
+    <script src="<?= base_url('');?>js/gdp-data.js"></script>
+    <script src="<?= base_url('');?>js/morris.min.js"></script>
+    <script src="<?= base_url('');?>js/sparklines.js"></script>
+    <script src="<?= base_url('');?>js/charts.js"></script>
+    <script src="<?= base_url('');?>js/jquery.slimscroll.min.js"></script>
     <script>
       //knob
       $(function() {
